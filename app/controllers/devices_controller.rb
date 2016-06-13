9d3f121f80c1dc2a0e535a4f9755dcdb16e60a39
arollick <arollick@profit.com.ua>
2016-06-13 21:38:37 +0300

    show devices

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,10 +1,13 @@
 class DevicesController < ApplicationController
-  
+
+  before_filter { @application = Application.find(params[:application_id].to_i) }
+  before_filter { params[:id] && @notification = @application.notifications.find(params[:id].to_i) }
+
   def show
     @device = Device.find(params[:id])
     @notifications = @device.notifications.includes(:auth_key => :environment).asc.page(params[:page])
   end
   def index
-    @devices = Device.asc
+    @devices = @application.devices.asc.includes(:auth_key => :environment).page(params[:page])
   end
 end
