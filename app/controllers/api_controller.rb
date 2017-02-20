5a701b9d02d55b9bbbcb11eb86067a1aa8a9a9d0
Adam Cooke <me@adamcooke.io>
2017-02-20 12:46:04 +0000

    add api methods for adding & removing auth keys

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,20 +3,9 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
   skip_before_action :login_required
 
-  before_action do
-    begin
-      @payload = JSON.parse(request.body.read).with_indifferent_access
-    rescue
-      json({:error => 'Invalid json payload'}, 400)
-      next
-    end
-
-    @auth_key = AuthKey.find_by_key(@payload[:auth_key])
-    if @auth_key.nil?
-      json({:error => 'Access denied'}, 403)
-      next
-    end
-  end
+  before_action :parse_json_payload
+  before_action :find_auth_key, :only => [:notify, :register]
+  before_action :find_application_api_key, :only => [:add_auth_key, :remove_auth_key]
 
   def notify
     notification = Notification.build_from_payload(@auth_key, @payload)
@@ -42,10 +31,52 @@ class ApiController < ApplicationController
     end
   end
 
+  def add_auth_key
+    auth_key = @application.auth_keys.build
+    auth_key.name = @payload['name']
+    auth_key.environment = @application.environments.where(:name => @payload['environment']).first
+    if auth_key.save
+      json({:status => 'ok', :key => auth_key.key})
+    else
+      json({:errors => auth_key.errors}, 422)
+    end
+  end
+
+  def remove_auth_key
+    auth_keys = @application.auth_keys.where(:key => @payload['key']).destroy_all
+    json({:status => 'ok', :keys_removed => auth_keys.size})
+  end
+
   private
 
   def json(object, status = 200)
     render :json => object, :status => status
+  end
+
+  def parse_json_payload
+    begin
+      @payload = JSON.parse(request.body.read).with_indifferent_access
+    rescue
+      json({:error => 'Invalid json payload'}, 400)
+    end
+  end
+
+  def find_auth_key
+    @auth_key = AuthKey.find_by_key(@payload[:auth_key])
+    if @auth_key.nil?
+      json({:error => 'Access denied'}, 403)
+    end
+  end
+
+  def find_application_api_key
+    if @payload[:api_key].present?
+      @application = Application.find_by_api_key(@payload[:api_key])
+      if @application.nil?
+        json({:error => 'Invalid application API key'}, 403)
+      end
+    else
+      json({:error => 'Missing application API key'}, 403)
+    end
   end
 
 end
