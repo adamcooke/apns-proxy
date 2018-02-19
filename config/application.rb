48691a90fa3cf7a67992f6aa8f7c63947b55fccf
Adam Cooke <me@adamcooke.io>
2018-02-19 17:07:17 +0000

    switch to eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module ApnsProxy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/lib)
     config.time_zone = 'UTC'
     config.generators do |g|
       g.orm             :active_record
